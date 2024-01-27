c9f7912526d7d346958d9576e25f132f9226f074
aSky17 <akashmishra.am.1710@gmail.com>
2024-01-27 09:59:57 +0530

    Completed User API

--- src/main/java/com/akash/blogApplication/controllers/UserController.java
+++ src/main/java/com/akash/blogApplication/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.akash.blogApplication.controllers;
 
+import com.akash.blogApplication.payloads.ApiResponse;
 import com.akash.blogApplication.payloads.UserDto;
 import com.akash.blogApplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/users")
@@ -25,7 +29,20 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable Integer userId) {
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<ApiResponse>
+                (new ApiResponse("User deleted successfully",true),HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 
 }
