2ae1d05899fae0a50c6d9ae334693bcff07c649b
aSky17 <akashmishra.am.1710@gmail.com>
2024-01-30 01:14:06 +0530

    Added validation and exception handler for the validation

--- src/main/java/com/akash/blogApplication/controllers/UserController.java
+++ src/main/java/com/akash/blogApplication/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.akash.blogApplication.controllers;
 import com.akash.blogApplication.payloads.ApiResponse;
 import com.akash.blogApplication.payloads.UserDto;
 import com.akash.blogApplication.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto createdUser = this.userService.createUser(userDto);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable Integer userId) {
         UserDto updatedUser = this.userService.updateUser(userDto,userId);
         return ResponseEntity.ok(updatedUser);
     }
--- src/main/java/com/akash/blogApplication/payloads/UserDto.java
+++ src/main/java/com/akash/blogApplication/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.akash.blogApplication.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +11,16 @@ import lombok.Setter;
 @Setter
 @Getter
 public class UserDto {
+
     private int id;
+    @NotEmpty
+    @Size(min = 4,message = "Username must be of minimum four characters")
     private String name;
+    @Email(message = "Given email address is not valid")
     private String email;
+    @NotEmpty
+    @Size(min = 7,message = "Password must of minimum 7 characters")
     private String password;
+    @NotEmpty
     private String about;
 }
